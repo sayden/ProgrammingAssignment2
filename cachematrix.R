73bc6d833e193270e15e0267a545bddab4da225f
Mario Castro <mariocaster@gmail.com>
2014-05-18 15:03:15 +0200

    Some comments added to improve readability

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Creates a Matrix pseudo-object with getter and setter as well
 ## as a inverse matrix variable with it's correspondig getter
 ## and setter
 makeCacheMatrix <- function(m = matrix())
 {
-    inv <- NULL
+    inv <- NULL             # Variable to hold the inverse matrix
+
+    # Sets the matrix to a new value and sets the inverse to NULL
     set <- function(y) {
         m <<- y
         inv <<- NULL
     }
+
+    #Gets the matrix currently stored
     get <- function() {
         m
     }
+
+    # Sets the inverse matrix
     setInv <- function(inverseMatrix) {
         inv <<- inverseMatrix
     }
+
+    #Gets the inverse matrix
     getInv <- function() {
         inv
     }
+
+    #Publish the available functions
     list(set = set, 
          get = get,
          setInv = setInv,
@@ -33,12 +40,15 @@ makeCacheMatrix <- function(m = matrix())
 ## returns the cached (stored) calculated version
 cacheSolve <- function(x, ...)
 {
+    # Stores a makeCacheMatrix object
     mat <- x$getInv()
     
+    # Check if there is a inverse matrix stored
     if(!is.null(mat)){
         message("Cached matrix stored, returning")
         return (mat)
     } else {
+        #When no cache is stored, calculate and set it
         message("No cache stored, solving inverse matrix")
         data <- x$get()
         mat <- solve(data)
@@ -46,7 +56,3 @@ cacheSolve <- function(x, ...)
         return(mat)
     }
 }
-
-a <- c(0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0)
-m <- matrix(a, nrow=5, byrow=TRUE)
-mPo <- makeCacheMatrix(m)
